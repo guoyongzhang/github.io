364eb8d923caefea11a98f69c1e63bd039636d6d
Guoyong Zhang <guoyong.zhang@oracle.com>
2021-08-09 12:16:47 -0400

    more changes

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 set -e
-PUBLISH_BRANCH=gh-pages
+PUBLISH_BRANCH=pages
 RELEASE_BRANCH=$1
 LATEST_VERSION=$2
 
+function resetRoot() {
+  gh-pages -b ${PUBLISH_BRANCH} -d hack
+}
+
 function ensureRoot() {
-    /usr/bin/gh-pages -b ${PUBLISH_BRANCH} -d hack -a
+  gh-pages -b ${PUBLISH_BRANCH} -d hack -a
 }
 
 if [[ ${RELEASE_BRANCH} == release-* ]]; then
   VERSION=v${RELEASE_BRANCH:8}
   echo "publish ${VERSION}"
-  /usr/bin/gh-pages -d production -b ${PUBLISH_BRANCH} -e "${VERSION}"
+  gh-pages -d production -b ${PUBLISH_BRANCH} -e "${VERSION}"
   if [[ "true" == "${LATEST_VERSION}" ]]; then
     echo "publish ${VERSION} as the latest"
-    /usr/bin/gh-pages -d production -b ${PUBLISH_BRANCH} -e latest
+    gh-pages -d production -b ${PUBLISH_BRANCH} -e latest
   fi
   ensureRoot
 elif [[ ${RELEASE_BRANCH} == master ]]; then
